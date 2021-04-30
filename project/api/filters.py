a1e0dba8205f5a782a5f29d19a559d0761a0fdea
joshegger <78809829+joshegger@users.noreply.github.com>
2021-04-30 14:02:58 +0200

    Update filters.py

--- project/api/filters.py
+++ project/api/filters.py
@@ -11,12 +11,13 @@ def group_by_filter(self, queryset, name, value):
     sort_by_date = CharFilter(method='sort_by_date_filter')
     sort_by_channel = CharFilter(method='sort_by_channel_filter')
     sort_by_country = CharFilter(method='sort_by_country_filter')
+    sort_by_spend = CharFilter(method='sort_by_spend_filter')
+    sort_by_revenue = CharFilter(method='sort_by_revenue_filter')
     sort_by_os = CharFilter(method='sort_by_os_filter')
     sort_by_impressions = CharFilter(method='sort_by_imperessions_filter')
     sort_by_clicks = CharFilter(method='sort_by_clicks_filter')
     sort_by_installs = CharFilter(method='sort_by_installs_filter')
-    sort_by_spend = CharFilter(method='sort_by_spend_filter')
-    sort_by_revenue = CharFilter(method='sort_by_revenue_filter')
+    
     sort_by_cpi = CharFilter(method='sort_by_cpi_filter')
 
 
@@ -27,64 +28,66 @@ class Filter_by_User(FilterSet):
     from_date=DateFilter(field_name='date',lookup_expr='gt')
     upto_date=DateFilter(field_name='date',lookup_expr='gt')
     date=DateFilter(field_name='date',lookup_expr='exact')
-    #group_by=CharFilter(method='group_by_filter')
+    
     group_by = group_by_filter
 
 
 
     def sort_by_date_filter(self,queryset,name,value):
-        if value=='desc':
+        if value == 'desc':
             return queryset.order_by('-date')
         else:
             return queryset.order_by('date')
 
     def sort_by_channel_filter(self,queryset,name,value):
-        if value=='desc':
+        if value == 'desc':
             return queryset.order_by('-channel')
         else:
             return queryset.order_by('channel')
 
     def sort_by_country_filter(self,queryset,name,value):
-        if value=='desc':
+        if value == 'desc':
             return queryset.order_by('-country')
         else:
             return queryset.order_by('country')
 
     def sort_by_os_filter(self,queryset,name,value):
-        if value=='desc':
+        if value == 'desc':
             return queryset.order_by('-os')
         else:
             return queryset.order_by('os')
-
+        
+    def sort_by_revenue_filter(self,queryset,name,value):
+        if value == 'desc':
+            return queryset.order_by('-revenue')
+        else:
+            return queryset.order_by('revenue')
+        
     def sort_by_impressions_filter(self,queryset,name,value):
-        if value=='desc':
+        if value == 'desc':
             return queryset.order_by('-imperessions')
         else:
             return queryset.order_by('impressions')
 
     def sort_by_clicks_filter(self,queryset,name,value):
-        if value=='desc':
+        if value == 'desc':
             return queryset.order_by('-clicks')
         else:
             return queryset.order_by('clicks')
 
     def sort_by_installs_filter(self,queryset,name,value):
-        if value=='desc':
+        if value == 'desc':
             return queryset.order_by('-installs')
         else:
             return queryset.order_by('install')
 
     def sort_by_spend_filter(self,queryset,name,value):
-        if value=='desc':
+        if value == 'desc':
             return queryset.order_by('-spend')
         else:
             return queryset.order_by('spend')
 
-    def sort_by_revenue_filter(self,queryset,name,value):
-        if value=='desc':
-            return queryset.order_by('-revenue')
-        else:
-            return queryset.order_by('revenue')
+
 
     def sort_by_cpi_filter(self,queryset,name,value):
         if value=='desc':
